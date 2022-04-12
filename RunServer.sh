f0bfc8619433349816ac55951e2d0ff5ae9f2372
nikhil <nikhil@blunt.money>
2022-04-12 10:28:50 +0530

    server url fixed

--- RunServer.sh
+++ RunServer.sh
@@ -5,4 +5,4 @@ kill -9 `ps aux | grep gunicorn | awk '{print $2}'` &
 pkill -f server.fastApi.service
 redis-server &
 python3 -m server.fastApi.services &
-gunicorn --bind 0.0.0.0:8000 server.fastApi.app:app -w 4 -k uvicorn.workers.UvicornWorker &
+gunicorn --bind 0.0.0.0:80 server.fastApi.app:app -w 4 -k uvicorn.workers.UvicornWorker &
